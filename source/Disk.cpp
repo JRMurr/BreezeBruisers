d4cea8972af32687a1f6b706530996d1b0abc9f9
Stefan Su <Stefan Su>
2016-04-10 01:27:17 -0400

    Fixed unanitalized a, fixed grab through wall

--- source/Disk.cpp
+++ source/Disk.cpp
@@ -89,7 +89,7 @@ void Disk::Draw(SDL_Renderer *screen) {
             src = sheet.getSprite(currentAnimation->getFrame(animTime));
         else
         {
-            int a;
+            int a = 0;
             float ang = atan2(yVel,xVel)*180/3.14;
             if(ang > 0){
                 if(ang < 22.5) a = 3;
@@ -169,7 +169,7 @@ void Disk::Update(int ticks) {
 			//crossed middle going right
 			grababble = true;
 		}
-		else if (x < WIDTH / 2 && xVel < 0) {
+		else if (x < WIDTH / 2 - width && xVel < 0) {
 			grababble = true;
 		}
 
